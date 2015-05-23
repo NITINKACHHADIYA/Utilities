e513a12beb7bf3e9322ab73a4ae01ef7d2c585f1
nitinkachhadiya <nitin.kahhadiyace@gmail.com>
2015-05-23 09:02:23 +0530

    Initial Commit

---
+++ Utilities/Utilities.h
@@ -0,0 +1,13 @@
+//
+//  Utilities.h
+//  Utilities
+//
+//  Created by OptiMatrix on 143/05/15.
+//  Copyright (c) 2015 YourCompany. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
+@end
---
+++ Utilities/Utilities.m
@@ -0,0 +1,13 @@
+//
+//  Utilities.m
+//  Utilities
+//
+//  Created by OptiMatrix on 143/05/15.
+//  Copyright (c) 2015 YourCompany. All rights reserved.
+//
+
+#import "Utilities.h"
+
+@implementation Utilities
+
+@end
